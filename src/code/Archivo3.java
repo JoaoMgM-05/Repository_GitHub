3b3a057f85204484c232f38a52f6b44a2fb39c46
Joao <149010998+JoaoMgM-05@users.noreply.github.com>
2024-03-24 14:43:44 +0100

    Update2 Archivo3.java
    Segundo método de tres.

--- src/code/Archivo3.java
+++ src/code/Archivo3.java
@@ -5,4 +5,10 @@ public class Archivo3 {
         UUID uuid = UUID.randomUUID();
         System.out.println("UUID generado: " + uuid);
     }
+  private static void otherGenerateUUID() {
+        String nombre = "Julia";
+        byte[] bytes = nombre.getBytes();
+        UUID uuid = UUID.nameUUIDFromBytes(bytes);
+        System.out.println("UUID generado desde el nombre: " + nombre + ": " +uuid);
+    }
 }
