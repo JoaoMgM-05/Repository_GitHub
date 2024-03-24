7cf6e8563231212e4c8fcb370120667910479401
Joao <149010998+JoaoMgM-05@users.noreply.github.com>
2024-03-24 14:40:55 +0100

    Update1 Archivo2.java
    Primer método de tres plus importar clase.

--- src/code/Archivo2.java
+++ src/code/Archivo2.java
@@ -1,5 +1,23 @@
 package code;
-
+import javax.swing.JOptionPane;
 public class Archivo2 {
 
+  private static int leerOpcion() {
+        int opcion;
+
+        System.out.println("----------------------------");
+        System.out.println("------MENU DE OPCIONES------");
+        System.out.println("----------------------------");
+        System.out.println("1)-----OBTENER POTENCIA-----");
+        System.out.println("2)-GENERAR NUMERO ALEATORIO-");
+        System.out.println("3)----CALCULAR FACTORIAL----");
+        System.out.println("4)----------SALIR-----------");
+        System.out.println("----------------------------");
+
+        opcion = Integer.parseInt(JOptionPane.showInputDialog(null, "MENU", "ELIGE UNA OPCIÓN"));
+
+        return opcion;
+
+    }
+  
 }
