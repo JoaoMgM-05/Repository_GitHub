a5c693cd36281d99dc4e5aefe6f0c615127fa97e
Joao <149010998+JoaoMgM-05@users.noreply.github.com>
2024-03-24 17:46:55 +0100

    Rama1_Archivo1_obtenerPortencia
    Creamos el método obtener potencia.

--- src/code/Archivo1.java
+++ src/code/Archivo1.java
@@ -1,5 +1,27 @@
 package code;
 import javax.swing.JOptionPane;
 public class Archivo1 {
-  
+  public static int obtenerPotencia(int answer) {
+
+        System.out.println("Aquí hallaremos la potencia de el primer número que me brindes elevado al segundo número que me brindes");
+
+        int a = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primer número"));
+        System.out.println("El primer número ingreado es: " + a);
+        System.out.println();
+        int b = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo número"));
+        System.out.println("El segundo número ingreado es: " + b);
+        System.out.println();
+
+        int potencia = (int)Math.pow(a, b);
+
+        answer = potencia;
+        System.out.println(" " + b);
+        System.out.print(a + " = ");
+        System.out.println(answer);
+        System.out.println();
+        System.out.println("Gracias por usar este método.");
+
+        return answer;
+
+    }
 }
