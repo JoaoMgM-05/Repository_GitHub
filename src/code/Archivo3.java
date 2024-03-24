9f6c04e0315eae1e4542660ec7cb715252468eb1
Joao <149010998+JoaoMgM-05@users.noreply.github.com>
2024-03-24 18:37:50 +0100

    rama2_Archivo3_generateUUID
    Creamos el método generador de UUID's.

--- src/code/Archivo3.java
+++ src/code/Archivo3.java
@@ -1,5 +1,8 @@
 package code;
 import java.util.UUID;
 public class Archivo3 {
-
+  private static void generateUUID() {
+        UUID uuid = UUID.randomUUID();
+        System.out.println("UUID generado: " + uuid);
+    }
 }
