657059a2ea8c8bed926065bdb63faeaac0b4ebfa
Joao <149010998+JoaoMgM-05@users.noreply.github.com>
2024-03-24 17:23:03 +0100

    Archivo3
    Desde cero, ya tiene la clase importada.

--- src/code/Archivo3.java
+++ src/code/Archivo3.java
@@ -1,19 +1,5 @@
 package code;
-
+import java.util.UUID;
 public class Archivo3 {
 
-  public static void main(String[] args) {
-        generateUUID();
-        otherGenerateUUID();
-    }
-  private static void generateUUID() {
-        UUID uuid = UUID.randomUUID();
-        System.out.println("UUID generado: " + uuid);
-    }
-  private static void otherGenerateUUID() {
-        String nombre = "Julia";
-        byte[] bytes = nombre.getBytes();
-        UUID uuid = UUID.nameUUIDFromBytes(bytes);
-        System.out.println("UUID generado desde el nombre: " + nombre + ": " +uuid);
-    }
 }
