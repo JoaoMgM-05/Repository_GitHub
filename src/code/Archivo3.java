12445dc862b96a99e695dc304c33cab8609d07f7
Joao <149010998+JoaoMgM-05@users.noreply.github.com>
2024-03-24 14:43:01 +0100

    Update1 Archivo3.java
    Primer método de tres

--- src/code/Archivo3.java
+++ src/code/Archivo3.java
@@ -1,5 +1,8 @@
 package code;
 
 public class Archivo3 {
-
+  private static void generateUUID() {
+        UUID uuid = UUID.randomUUID();
+        System.out.println("UUID generado: " + uuid);
+    }
 }
