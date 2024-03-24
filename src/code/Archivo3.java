a5e1c05b61f4c4d157ac0e258c4ab45ad47711c9
Joao <149010998+JoaoMgM-05@users.noreply.github.com>
2024-03-24 18:46:04 +0100

    rama2_Archivo3_otherGenerateUUID
    Creamos un método que genera código UUID.

--- src/code/Archivo3.java
+++ src/code/Archivo3.java
@@ -2,4 +2,10 @@ package code;
 import java.util.UUID;
 public class Archivo3 {
 
+  private static void otherGenerateUUID() {
+        String nombre = "Julia";
+        byte[] bytes = nombre.getBytes();
+        UUID uuid = UUID.nameUUIDFromBytes(bytes);
+        System.out.println("UUID generado desde el nombre: " + nombre + ": " +uuid);
+    }
 }
