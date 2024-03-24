80338b593328b558eeb3e14bf34ff42234f13d82
Joao <149010998+JoaoMgM-05@users.noreply.github.com>
2024-03-24 18:40:00 +0100

    rama2_Archivo3_main
    Llamamos al método generateUUID desde el main.

--- src/code/Archivo3.java
+++ src/code/Archivo3.java
@@ -1,6 +1,10 @@
 package code;
 import java.util.UUID;
 public class Archivo3 {
+
+  public static void main(String[] args) {
+    generateUUID();
+  }
   private static void generateUUID() {
         UUID uuid = UUID.randomUUID();
         System.out.println("UUID generado: " + uuid);
